74ac602f2352c42984661cdb3cc2caf12517a18f
Risent Zhang <shengqi542@gmail.com>
2014-06-08 15:13:11 +0800

    Add background color params support.

--- placeholder.go
+++ placeholder.go
@@ -1,28 +1,83 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"strings"
-	"strconv"
-	"io/ioutil"
-	"net/http"
-	"image"
-	"image/jpeg"
-	"image/draw"
-	"image/color"
 	"code.google.com/p/freetype-go/freetype"
+	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/jpeg"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
 var (
-        imageQuality = jpeg.Options{95}
-	blue  color.Color = color.RGBA{170, 170, 170, 1}
-	fontSize = 30
-	fontSpacing = 1.5
+	imageQuality             = jpeg.Options{95}
+	bgColor         color.Color = color.RGBA{170, 170, 170, 1}
+	fontSize                 = 30
+	fontSpacing              = 1.5
 )
 
+/* ************************************************************************** */
+// Copy from https://code.google.com/p/gorilla/source/browse/color/hex.go
+
+// HexModel converts any color.Color to an Hex color.
+var HexModel = color.ModelFunc(hexModel)
+
+// Hex represents an RGB color in hexadecimal format.
+//
+// The length must be 3 or 6 characters, preceded or not by a '#'.
+type Hex string
+
+// RGBA returns the alpha-premultiplied red, green, blue and alpha values
+// for the Hex.
+func (c Hex) RGBA() (uint32, uint32, uint32, uint32) {
+	r, g, b := HexToRGB(c)
+	return uint32(r) * 0x101, uint32(g) * 0x101, uint32(b) * 0x101, 0xffff
+}
+
+// hexModel converts a color.Color to Hex.
+func hexModel(c color.Color) color.Color {
+	if _, ok := c.(Hex); ok {
+		return c
+	}
+	r, g, b, _ := c.RGBA()
+	return RGBToHex(uint8(r>>8), uint8(g>>8), uint8(b>>8))
+}
+
+// RGBToHex converts an RGB triple to an Hex string.
+func RGBToHex(r, g, b uint8) Hex {
+	return Hex(fmt.Sprintf("#%02X%02X%02X", r, g, b))
+}
+
+// HexToRGB converts an Hex string to a RGB triple.
+func HexToRGB(h Hex) (uint8, uint8, uint8) {
+	if len(h) > 0 && h[0] == '#' {
+		h = h[1:]
+	}
+	if len(h) == 3 {
+		h = h[:1] + h[:1] + h[1:2] + h[1:2] + h[2:] + h[2:]
+	}
+	if len(h) == 6 {
+		if rgb, err := strconv.ParseUint(string(h), 16, 32); err == nil {
+			return uint8(rgb >> 16), uint8((rgb >> 8) & 0xFF), uint8(rgb & 0xFF)
+		}
+	}
+	return 0, 0, 0
+}
+/* ************************************************************************** */
+
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	size := strings.Split(r.URL.Path[1:], "x")
+	params := strings.Split(r.URL.Path[1:], "/")
+	if len(params) == 2 {
+		r, g, b := HexToRGB(Hex(params[1]))
+		bgColor = color.RGBA{r, g, b, 1}
+	}
+	size := strings.Split(params[0], "x")
 	if len(size) < 2 {
 		return
 	}
@@ -34,9 +89,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		height = 0
 	}
-	
+
 	m := image.NewRGBA(image.Rect(0, 0, width, height))
-	draw.Draw(m, m.Bounds(), &image.Uniform{blue}, image.ZP, draw.Src)
+	draw.Draw(m, m.Bounds(), &image.Uniform{bgColor}, image.ZP, draw.Src)
 
 	fontBytes, err := ioutil.ReadFile("/usr/share/fonts/TTF/luxisr.ttf")
 	font, err := freetype.ParseFont(fontBytes)
@@ -54,8 +109,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	c.SetHinting(freetype.FullHinting)
 
 	text := fmt.Sprintf("%d X %d", width, height)
-	x0 := width/2 - len(text) * fontSize/3
-	y0 := height/2
+	x0 := width/2 - len(text)*fontSize/3
+	y0 := height / 2
 	pt := freetype.Pt(x0, y0)
 	c.DrawString(text, pt)
 	w.Header().Set("Content-Type", "image/jpeg")
